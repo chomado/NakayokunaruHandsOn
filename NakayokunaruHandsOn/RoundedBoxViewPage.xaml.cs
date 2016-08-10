ce9e81e1e92754ed12807ae07b17f7d9253570d4
chomado <chomado@gmail.com>
2016-08-10 19:54:52 +0900

    ContentPage「RoundedBoxViewPage」を追加

---
+++ NakayokunaruHandsOn/RoundedBoxViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NakayokunaruHandsOn
+{
+    public partial class RoundedBoxViewPage : ContentPage
+    {
+        public RoundedBoxViewPage()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            roundedBox.Color = Color.FromRgb(
+                random.Next(255),
+                random.Next(255),
+                random.Next(255));
+        }
+    }
+}
